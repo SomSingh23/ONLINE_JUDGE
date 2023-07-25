e8acdd69b11a36e3c81af0cc4d1438b7a013c916
SomSingh23 <githubsom@gmail.com>
2023-07-25 14:16:39 +0530

    modified runPy to handle inf loops and errors

--- backend/runcode/runPy.js
+++ backend/runcode/runPy.js
@@ -2,16 +2,22 @@ const child_process = require("child_process");
 const runPy = async (filePath, inputPath) => {
   const runPython = `python ${filePath}<${inputPath}`;
   return new Promise((resolve, reject) => {
+    let timeOutId = setTimeout(() => {
+      console.log("Checking for INF loops or File handing");
+      reject("");
+    }, 4000);
     child_process.exec(runPython, (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${error.message}`);
+        clearTimeout(timeOutId);
         reject(error.message);
       }
       if (stderr) {
         console.error(`stderr: ${stderr}`);
+        clearTimeout(timeOutId);
         reject(stderr);
       }
-      resolve(stdout);
+      resolve(stdout, clearTimeout(timeOutId));
     });
   });
 };
